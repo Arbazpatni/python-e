1f5651838f61eb9be79e91a7b974c184567d80a5
Arbazpatni <sowmiyancloud@gmail.com>
2026-04-02 15:41:43 +0530

    update 6

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     # This will show 'user1', 'user2', etc. so you know which one you are on
     pa_user = os.environ.get('USER', 'Unknown User')
-    return f"<h1>Hello World!</h1><p>Running on PythonAnywhere account: <b>{pa_user}</b></p><p>This is reflect update 5</p>"
+    return f"<h1>Hello World!</h1><p>Running on PythonAnywhere account: <b>{pa_user}</b></p><p>This is update 6</p>"
 
 @app.route('/pulse')
 def pulse():
