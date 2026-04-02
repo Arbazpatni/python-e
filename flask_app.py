d78eacec300a7b60742d0250b3b38327524e36f0
Arbazpatni <sowmiyancloud@gmail.com>
2026-04-02 12:44:30 +0530

    first commit

---
+++ flask_app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # This will show 'user1', 'user2', etc. so you know which one you are on
+    pa_user = os.environ.get('USER', 'Unknown User')
+    return f"<h1>Hello World!</h1><p>Running on PythonAnywhere account: <b>{pa_user}</b></p>"
+
+@app.route('/pulse')
+def pulse():
+    return "Alive", 200
